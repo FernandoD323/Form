a5638461e09aae846efa79e5d0f4a1d922eb2d6b
Fernando Diaz <diazemanuel323@gmail.com>
2022-09-09 08:52:35 -0600

    Asignación correcta que va a tener cada campo en los primeros 3 forms

--- form/src/Form.jsx
+++ form/src/Form.jsx
@@ -13,27 +13,34 @@ export const Form = () => {
         <input type="text" class="form__email input" placeholder="Correo:"/>
         <input type="text" class="form__email input" placeholder="Teléfono:"/>
         <input type="text" class="form__email input" placeholder="Dirección:"/>
-        <input type="text" class="form__email  input1" placeholder="Carnet:"/>
+        <input type="text" class="form__email  input" placeholder="Carnet:"/>
+        <input type="text" class="form__email  input1" placeholder="Tutor legal/Encargad@:"/>
         <input type="submit" value="Siguiente" class="form__submit"/>
         
     </form>
 
    <form class="form">
         <h2 class="form__title"> - Datos Familiares - </h2>
-        <input type="text" class="form__name input" placeholder="Nombres Madre:"/>
-        <input type="email" class="form__email input" placeholder="Nombres Padre:"/>
-
-        {/* <textarea class="form__message input" placeholder="Mensaje:"></textarea> */}
+        <input type="text" class="form__name input" placeholder="Nombres y Apellidos de la Madre:"/>
+        <input type="email" class="form__email input" placeholder="Nombres y apellidos del Padre:"/>
+        <input type="email" class="form__email input" placeholder="Teléfono de la Madre"/>
+        <input type="text" class="form__name input" placeholder="Teléfono del Padre"/>
+        <input type="email" class="form__email input" placeholder="Fecha de Nacimiento de la Madre"/>
+        <input type="email" class="form__email input" placeholder="Fecha de Nacimiento del Padre"/>
+        <input type="email" class="form__email input" placeholder="Ocupación de la Madre"/>
+        <input type="email" class="form__email input" placeholder="Ocupación del Padre"/>
+        <input type="email" class="form__email input1" placeholder="¿Cuántos Herman@s Tiene?"/>
+        {/* <textarea class="form__message input" placeholder="Tiene:"></textarea> */}
         <input type="submit" value="Atras" class="form__submit"/>
         <input type="submit" value="Siguiente" class="form__submit"/>
     </form>
 
     <form class="form">
         <h2 class="form__title"> - Ingresos - </h2>
-        <input type="text" class="form__name input" placeholder="Nombres Madre:"/>
-        <input type="email" class="form__email input" placeholder="Nombres Padre:"/>
-
-        {/* <textarea class="form__message input" placeholder="Mensaje:"></textarea> */}
+        <input type="text" class="form__name input" placeholder="Ingresos de la Madre:"/>
+        <input type="email" class="form__email input" placeholder="Ingresos del Padre:"/>
+        <input type="email" class="form__email input2" placeholder="Ingresos de los hermanos:"/>
+        <textarea class="form__message input" placeholder="Detalle si tiene alguna otra forma en la que obtiene algún ingreso:"></textarea>
         <input type="submit" value="Atras" class="form__submit"/>
         <input type="submit" value="Siguiente" class="form__submit"/>
     </form>
@@ -43,7 +50,7 @@ export const Form = () => {
         <input type="text" class="form__name input" placeholder="Nombres Madre:"/>
         <input type="email" class="form__email input" placeholder="Nombres Padre:"/>
 
-        {/* <textarea class="form__message input" placeholder="Mensaje:"></textarea> */}
+        
         <input type="submit" value="Atras" class="form__submit"/>
         <input type="submit" value="Siguiente" class="form__submit"/>
     </form>
